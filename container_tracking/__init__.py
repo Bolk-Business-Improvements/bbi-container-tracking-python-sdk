2e375eb0c1db5336ae03009bc2bc5142c9218e4b
bolk-berdal <r.loof@bolk.com>
2025-08-27 09:19:35 +0200

    Export schemas in container_tracking and minor main.py edit
    Added exports for multiple schema classes in container_tracking/__init__.py for easier imports. Also added a blank line at the end of main.py to comply with style guidelines.

--- container_tracking/__init__.py
+++ container_tracking/__init__.py
@@ -1,1 +1,15 @@
 from container_tracking.client import BBIContainerTracking
+from container_tracking.schemas import (
+    Carrier,
+    Container,
+    Country,
+    Location,
+    Movement,
+    PortOfDischarge,
+    PortOfLoading,
+    Route,
+    Shipment,
+    ShipmentBase,
+    ShipmentCreate,
+    Vessel,
+)
